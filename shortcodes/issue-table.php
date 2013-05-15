ce7b319b3f763e1d40691cee96deb45a43409a7c
Henry-01 <hendrik.boone1@gmail.com>
2013-05-15 12:57:36 +0200

    only load css when table is shown
    Change-Id: Ia44ebed4c7d2346054dba355c717f97cf7620d89

--- shortcodes/issue-table.php
+++ shortcodes/issue-table.php
@@ -1,20 +1,24 @@
 <?php
-function issue_table_style() {
-	wp_enqueue_style('really-simple-issue-tracker-table-style', plugin_dir_url(__FILE__).'issue-table.css');
-}
+
+function issue_table_style() {wp_register_style('issue-table-style', plugin_dir_url(__FILE__).'issue-table.css');}
 add_action('wp_enqueue_scripts', 'issue_table_style');
 
 function issue_table() { 
-	$querytype = 'post_type="issue"';
+	wp_enqueue_style('issue-table-style');
+
+	$querystring = 'post_type="issue"&showposts=20';
+	
 	echo '<table id="issue-table"><thead><tr><th>Date</th><th>Type</th><th>Status</th><th>Title</th><th>Project</th><th>For</th><th>Priority</th><th>Spent</th><th>Assigned</th></tr></thead>';
 	echo '<tbody>';
-	query_posts($querytype);
-
+	
+	query_posts($querystring);
 	while ( have_posts() ) : the_post();
 	$post = get_the_ID();
+    
     echo '<tr><td>';
     echo get_the_date();
-
+	echo '</td><td>';
+    
     $issue = get_post_meta($post, 'issue_type', true);
     $issue_types = ReallySimpleIssueTracker_IssueType::getDefaultIssueTypes();
 	if($issue) {
@@ -23,19 +27,22 @@ function issue_table() {
                 if($type->getIssueTypeById($issue))
             	$issue_type = $type;
     			}
-            	echo '<td>'.$issue_type->getName().'</td>';
+            	echo $issue_type->getName();
             }
+    echo '</td>';
 
     $status = get_post_meta($post,'issue_status', true);
     $status_types = ReallySimpleIssueTracker_Status::getDefaultStatusTypes();
-        if($status) {
-            $status_type = object;
+    if($status) {
+        $status_type = object;
             foreach($status_types as $type) {
                 if($type->getStatusTypeById($status))
             	$status_type = $type;
     			}
-            	echo '<td class="'.$status_type->getId().'">'.$status_type->getName().'';
+            	echo '<td class="'.$status_type->getId().'">'.$status_type->getName();
             }
+    else {echo '<td>';}
+
     echo '</td><td>';
 ?>
     <a href=<?php the_permalink() ?>><?php the_title() ?></a>
@@ -48,6 +55,7 @@ function issue_table() {
     	if($assigned) {echo $assigned;}
     	else {echo __('Unassigned',ReallySimpleIssueTracker::HANDLE);}
 
+	echo '</td><td>';
 	$priority = get_post_meta($post, 'priority', true);
     $priority_types = ReallySimpleIssueTracker_Priority::getDefaultPriorities();
 	if($priority) {
@@ -56,7 +64,7 @@ function issue_table() {
                 if($type->getPriorityById($priority))
             	$priority_type = $type;
     			}
-            	echo '</td><td>'.$priority_type->getName().'';
+            	echo $priority_type->getName();
             }
     echo '</td><td>';
     $time_spent = get_post_meta($post, 'time_spent', true);
@@ -67,8 +75,9 @@ function issue_table() {
     echo '</td></tr>';
 	
 	endwhile;
-	echo '</tbody></table>';
+	
 	wp_reset_query(); 
+	echo '</tbody></table>';
 }
 add_shortcode('issue-table', 'issue_table');
 ?>
